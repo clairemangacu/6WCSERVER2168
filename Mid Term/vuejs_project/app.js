26ce7922a84aea2aea6ad7b2d33adac778ec2571
clairemangacu <claireannemangacu27@gmail.com>
2025-08-29 12:35:04 +0800

    Add files via upload

---
+++ Mid Term/vuejs_project/app.js
@@ -0,0 +1,19 @@
+ //const app = Vue.createApp({
+
+         //Create functions, data
+    //template: "<h2>This is my template...</h2>"
+//});
+
+//app.mount('#app')
+
+const app = Vue.createApp({
+    // Create functions, data
+   data(){
+        return {
+    title: 'The Ledger',
+    author: 'Joseph Esquivel',
+    age: 34
+    }
+  }
+}
+);
